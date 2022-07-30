2f6b159ce1f05518526c0c05406f546b402f9512
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-07-30 11:44:47 -0300

    Rafatorando códigos para ajustes de ';' e espaçamento

--- src/challenges.js
+++ src/challenges.js
@@ -2,24 +2,24 @@
 const girafa = true;
 const elefante = true;
 const macaco = false;       
-function compareTrue(valor1, valor2){
+function compareTrue(valor1, valor2) {
     resultado1 = false;
     resultado2 = false;
     resultadoGeral = false;
 
-    if (valor1 === true){
+    if (valor1 === true) {
         resultado1 = true;
     } else {
         resultado1 = false;
     }
 
-    if (valor2 === true){
+    if (valor2 === true) {
         resultado2 = true;
     } else {
         resultado2 = false;
     }
 
-    if (resultado1 === true && resultado2 === true){
+    if (resultado1 === true && resultado2 === true) {
         resultadoGeral = true;
     }  
 
@@ -53,15 +53,15 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-  let maiorNumero = array[0]; //variável que irá receber o maior numero do array
-  let repeticao = 0; //variável que vai receber a quantidade de repeticões
-  for (let index in array){ //primeiro for para definir quem é o maiorNumero
-    if(maiorNumero < array[index]){
-      maiorNumero = array[index]
+  let maiorNumero = array[0]; // variável que irá receber o maior numero do array
+  let repeticao = 0; // variável que vai receber a quantidade de repeticões
+  for (let index in array){ // primeiro for para definir quem é o maiorNumero
+    if(maiorNumero < array[index]) {
+      maiorNumero = array[index];
     }
   }
-  for (let contador in array){ //segundo for para definir repetição do maiorNumero
-    if(maiorNumero === array[contador]){
+  for (let contador in array) { //segundo for para definir repetição do maiorNumero
+    if(maiorNumero === array[contador]) {
       repeticao += 1;
     }
   }
@@ -84,7 +84,7 @@ function catAndMouse(mouse, cat1, cat2) {
   } else {
     distancia2 = (mouse - cat2);
   }
-  if(distancia1 < distancia2){
+  if(distancia1 < distancia2) {
     return ('cat1');
   } else if (distancia2 < distancia1) {
     return ('cat2');
@@ -97,15 +97,15 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(array) {
   let resultado = [];
 
-  for (let index = 0; index < array.length; index ++){
-    if(array[index] % 3 === 0 && array[index] % 5 === 0){
+  for (let index = 0; index < array.length; index ++) {
+    if(array[index] % 3 === 0 && array[index] % 5 === 0) {
       resultado[index] = 'fizzBuzz';
-    } else if (array[index] % 3 === 0){
+    } else if (array[index] % 3 === 0) {
       resultado[index] = 'fizz';
-    } else if (array[index] % 5 === 0){
+    } else if (array[index] % 5 === 0) {
       resultado[index] = 'buzz';
     } else {
-      resultado[index] = 'bug!'
+      resultado[index] = 'bug!';
     }
   }
   return (resultado);
@@ -114,69 +114,61 @@ function fizzBuzz(array) {
 // Desafio 9
 function encode(string) {
   let stringToArray = string.split("");
-
-    for(let index in stringToArray){
-        if(stringToArray[index] === 'a'){
+    for(let index in stringToArray) {
+        if(stringToArray[index] === 'a') {
             stringToArray[index] = '1';
-        } else if (stringToArray[index] === 'e'){
+        } else if (stringToArray[index] === 'e') {
             stringToArray[index] = '2';
-        } else if (stringToArray[index] === 'i'){
+        } else if (stringToArray[index] === 'i') {
             stringToArray[index] = '3';
-        } else if (stringToArray[index] === 'o'){
+        } else if (stringToArray[index] === 'o') {
             stringToArray[index] = '4';
-        } else if (stringToArray[index] === 'u'){
+        } else if (stringToArray[index] === 'u') {
             stringToArray[index] = '5';
         } else {
             stringToArray[index] = stringToArray[index];
         }
     }
-
-    let arrayToString = stringToArray.join('')
+    let arrayToString = stringToArray.join('');
     return (arrayToString);
-}
-  
+}  
 function decode(string) {
   let stringToArray = string.split("");
-
-  for(let index in stringToArray){
-      if(stringToArray[index] === '1'){
+  for(let index in stringToArray) {
+      if(stringToArray[index] === '1') {
           stringToArray[index] = 'a';
-      } else if (stringToArray[index] === '2'){
+      } else if (stringToArray[index] === '2') {
           stringToArray[index] = 'e';
-      } else if (stringToArray[index] === '3'){
+      } else if (stringToArray[index] === '3') {
           stringToArray[index] = 'i';
-      } else if (stringToArray[index] === '4'){
+      } else if (stringToArray[index] === '4') {
           stringToArray[index] = 'o';
-      } else if (stringToArray[index] === '5'){
+      } else if (stringToArray[index] === '5') {
           stringToArray[index] = 'u';
       } else {
           stringToArray[index] = stringToArray[index];
       }
   }
-
-  let arrayToString = stringToArray.join('')
+  let arrayToString = stringToArray.join('');
   return (arrayToString);
 }
-
-console.log(decode('h3 th2r2!'));
+// console.log(decode('h3 th2r2!'));
 
 // Desafio 10
 function techList(techs, name) {
   let array = techs;
   let nome = name;
   let arraySorted = array.sort();
-  let arrayObjetos = [];
-  
-  if(array.length === 0){
-      return ("Vazio!")
+  let arrayObjetos = [];  
+  if(array.length === 0) {
+      return ("Vazio!");
   } else {
-      for (let index = 0; index < arraySorted.length; index ++){
+      for (let index = 0; index < arraySorted.length; index ++) {
           arrayObjetos.push({tech: arraySorted[index], name: nome})
       }
       return(arrayObjetos);
   }  
 }
-
 module.exports = {
   calcArea,
   catAndMouse,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,68 +1,63 @@
 // Desafio 11
 function generatePhoneNumber(array) {
   //dividindo o problema:
-//1 - o array de numeros deverá retornar uma string no formato de um número de telefone
-//2 - nenhum numero do array poderá ser maior que 9
-//3 - nenhum numero do array poderá ser menor que zero
-//4 - nenhum numero do array poderá se repetir por 3 vezes ou mais
-//5 - o array não poderá ter mais e nem menos de 11 números
-//6 - caso não cumpra os requisitos 2, 3 e 4, deverá retornar a mensagem: 'não é possível gerar um número de telefone com esses valores'
-//7 - caso o array tenha mais de 11 números ou menos de 11 números, deverá retornar a mensagem: 'Array com tamanho incorreto.' 
+// 1 - o array de numeros deverá retornar uma string no formato de um número de telefone
+// 2 - nenhum numero do array poderá ser maior que 9
+// 3 - nenhum numero do array poderá ser menor que zero
+// 4 - nenhum numero do array poderá se repetir por 3 vezes ou mais
+// 5 - o array não poderá ter mais e nem menos de 11 números
+// 6 - caso não cumpra os requisitos 2, 3 e 4, deverá retornar a mensagem: 'não é possível gerar um número de telefone com esses valores'
+// 7 - caso o array tenha mais de 11 números ou menos de 11 números, deverá retornar a mensagem: 'Array com tamanho incorreto.' 
   let maiorNumero = array[0];
   let menorNumero = array[0];
   let statusTamanho = false;
   let statusNumero = false;
-
-  if (array.length > 11 || array.length < 11){
+  if (array.length > 11 || array.length < 11) {
       statusTamanho = false;
-      return('Array com tamanho incorreto.')
+      return('Array com tamanho incorreto.');
   } else {
       statusTamanho = true;
       for (let index in array){
-          if (maiorNumero < array[index]){ //estrutura para identificar maior número do array
+          if (maiorNumero < array[index]) { //estrutura para identificar maior número do array
               maiorNumero = array[index];
           }
-          if (menorNumero > array[index]){ //estrutura para identificar menor número do array
+          if (menorNumero > array[index]) { //estrutura para identificar menor número do array
               menorNumero = array[index];
           }
       };
   }
-  if (maiorNumero > 9 || menorNumero < 0){
+  if (maiorNumero > 9 || menorNumero < 0) {
       statusNumero = false;
       return('não é possível gerar um número de telefone com esses valores')
   } else {
       statusNumero = true;
   }
   //Verificando repetições
-
   let repeticao = 0;
   let contNumero = 0;
   let statusRepeticao = false;
-
-  for (var atual in array){    
-      for (var prox in array){
-        if (array[atual] === array[prox]){
-          contNumero += 1
+  for (var atual in array) {    
+      for (var prox in array) {
+        if (array[atual] === array[prox]) {
+          contNumero += 1;
         }      
       }
-      if (contNumero > repeticao){
+      if (contNumero > repeticao) {
           repeticao = contNumero;
         }
       contNumero = 0;
     }
-  if (repeticao < 3){
+  if (repeticao < 3) {
       statusRepeticao = true;
   } else {
       return('não é possível gerar um número de telefone com esses valores');
   }
-
   let numeroTelefone = "";
-
-  if (statusTamanho === true && statusNumero === true && statusRepeticao === true){
-      for(let contador in array){
+  if (statusTamanho === true && statusNumero === true && statusRepeticao === true) {
+      for(let contador in array) {
           numeroTelefone += array[contador];
       }    
-      return("(" + numeroTelefone[0] + numeroTelefone[1] + ") " + numeroTelefone[2] + numeroTelefone[3] + numeroTelefone[4] + numeroTelefone[5] + numeroTelefone[6] + "-" + numeroTelefone[7] + numeroTelefone[8] + numeroTelefone[9] + numeroTelefone[10]);
+      return('(' + numeroTelefone[0] + numeroTelefone[1] + ') ' + numeroTelefone[2] + numeroTelefone[3] + numeroTelefone[4] + numeroTelefone[5] + numeroTelefone[6] + '-' + numeroTelefone[7] + numeroTelefone[8] + numeroTelefone[9] + numeroTelefone[10]);
   }
 }
 
@@ -78,10 +73,8 @@ function triangleCheck(lineA, lineB, lineC) {
 
   // A função triangleCheck deve retornar o valor true quando a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois lados e maior que o valor absoluto da diferença entre os outros dois lados.
 
-  //Opção 01: cirando variáveis para as somas e substrações, limitado a 3 linhas:
-
+  // Opção 01: cirando variáveis para as somas e substrações, limitado a 3 linhas:
   // let arrayLinhas = [lineA, lineB, lineC];
-
   // let somaAB = lineA + lineB;
   // let somaAC = lineA + lineC;
   // let somaBC = lineB + lineC;
@@ -89,7 +82,7 @@ function triangleCheck(lineA, lineB, lineC) {
   // let minusAC = Math.abs (lineA - lineC);
   // let minusBC = Math.abs (lineA - lineC);
   // let statusTamanhoLinhas = false;
-  //   for (var index in arrayLinhas){
+  //   for (var index in arrayLinhas) {
   //     let linhaAtual = arrayLinhas[index];
   //     if (linhaAtual > somaAB || linhaAtual > somaAC || linhaAtual > somaBC) {
   //         statusTamanhoLinhas = false;
@@ -103,37 +96,32 @@ function triangleCheck(lineA, lineB, lineC) {
   // }
   // triangleCheck(10, 14, 8);
 
-  //Opção 2: aplicando estrutura loop for para realizar operações com números independente da quantidade de linhas:
-  
-  let arrayLinhas = [lineA, lineB, lineC];
-  
+  // Opção 2: aplicando estrutura loop for para realizar operações com números independente da quantidade de linhas:  
+  let arrayLinhas = [lineA, lineB, lineC];  
   let primeiraSoma = arrayLinhas[0] + arrayLinhas[1];
   let somaMinima = primeiraSoma;
   let primeiraSubtracao = arrayLinhas[0] - arrayLinhas[1];
   let subtracaoMaxima = Math.abs (primeiraSubtracao);
   let statusTamanho = false;
-
-  for (var index = 0; index < arrayLinhas.length -1; index ++){
+  for (var index = 0; index < arrayLinhas.length -1; index ++) {
     let linhaAtual = arrayLinhas[index];
-
-    //for para encontrar a menor soma, já que as linhas devem ser menor que o somatório das outras duas
-    for (var index2 = index + 1; index2 < arrayLinhas.length; index2 ++){
+    // for para encontrar a menor soma, já que as linhas devem ser menor que o somatório das outras duas
+    for (var index2 = index + 1; index2 < arrayLinhas.length; index2 ++) {
       let proximaLinha = arrayLinhas[index2];
-      if (somaMinima > linhaAtual + proximaLinha){
+      if (somaMinima > linhaAtual + proximaLinha) {
         somaMinima = linhaAtual + proximaLinha;
       }
     }
-    for (var index2 = index + 1; index2 < arrayLinhas.length; index2 ++){
+    for (var index2 = index + 1; index2 < arrayLinhas.length; index2 ++) {
       let proximaLinha = arrayLinhas[index2];
-      if (subtracaoMaxima < Math.abs (linhaAtual - proximaLinha)){
+      if (subtracaoMaxima < Math.abs (linhaAtual - proximaLinha)) {
           subtracaoMaxima = Math.abs (linhaAtual - proximaLinha);
       }
     }
   }
-
-  for (var index = 0; index < arrayLinhas.length; index ++){
+  for (var index = 0; index < arrayLinhas.length; index ++) {
       let linhaAtual = arrayLinhas[index];
-      if (linhaAtual < somaMinima && linhaAtual > subtracaoMaxima){
+      if (linhaAtual < somaMinima && linhaAtual > subtracaoMaxima) {
           statusTamanho = true;
       }
   }
@@ -143,34 +131,27 @@ triangleCheck(10, 14, 8);
 
 // Desafio 13
 function hydrate(string) {
-  //O método para extrair números de uma string, foi encontrado através do canal All Things JavaScript, LLC. Que pode ser visto através do link: https://www.youtube.com/watch?v=pfkkdzeyx6U 
+  // O método para extrair números de uma string, foi encontrado através do canal All Things JavaScript, LLC. Que pode ser visto através do link: https://www.youtube.com/watch?v=pfkkdzeyx6U 
 
   let reg = /\d+/g;
-
   let arrayStringNumeros = string.match(reg);
-
   let arrayNumeros = [];
-
   let coposDeAgua = 0;
-
-  for (var index = 0; index < arrayStringNumeros.length; index ++){
+  for (var index = 0; index < arrayStringNumeros.length; index ++) {
       let numeroAtual = parseInt (arrayStringNumeros[index]);
       arrayNumeros.push(numeroAtual);
-      coposDeAgua += numeroAtual
+      coposDeAgua += numeroAtual;
   }
-  let resultado = coposDeAgua
-
-  if (coposDeAgua === 1){
-    resultado = coposDeAgua + ' copo de água'
+  let resultado = coposDeAgua;
+  if (coposDeAgua === 1) {
+    resultado = coposDeAgua + ' copo de água';
   } else {
-    resultado = coposDeAgua + ' copos de água'
+    resultado = coposDeAgua + ' copos de água';
   }
-
   // console.log(arrayStringNumbers);
   // console.log(arrayNumeros);
   // console.log(coposDeAgua);
-  // console.log(resultado);
-  
+  // console.log(resultado);  
   return (resultado);
 }
 
